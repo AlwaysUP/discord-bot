09817fba1fce729c487e88d35f5fc2abbef702e9
AlwaysUP <AlwaysUP@github.com>
2020-08-15 16:24:58 -0400

    add node server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+var http = require('http');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -12,3 +13,8 @@ client.on('message', msg => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+http.createServer(function (req, res) {
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+  }).listen(process.env.PORT || 5000);
